89ff917f0e1acd5440ca8943863938125dc37358
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-08-01 10:33:56 -0600

    Add basic Login template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import "./App.css";
+import LoginPage from "./Components/LoginPage";
 import Navibar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navibar/>
+      <LoginPage/>
     </div>
+    
   );
 }
 
---
+++ src/Components/LoginPage.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+
+function LoginPage(props) {
+    const [isSubmit, setSubmit] = useState(false);
+    
+    const account = [
+        {
+            email: 'cprg352+admin@gmail.com',
+            password: 'password'
+        },
+        {
+            email: 'cprg352+admin2@gmail.com',
+            password: 'password'
+        },
+        {
+            email: 'cprg352+anne@gmail.com',
+            password: 'password'
+        },
+        {
+            email: 'cprg352+barb@gmail.com',
+            password: 'password'
+        }
+    ];
+
+    const submitHandle = (event) => {
+        event.preventDefault();
+
+        var { email, password } = document.forms[0];
+
+        const userInfo = users.find((user) => user.email === email.value)
+
+        if (userInfo) {
+            if (userInfo.password !== password.value) {
+                alert("Invalid password")
+            } else {
+                setSubmit(true);
+                alert(`Email: ${userInfo.email} \nPassword: ${userInfo.password}`)
+            }
+        } else {
+            alert("Invalid username")
+        }
+    }
+
+    const SignInForm = (
+        <div className="loginForm">
+            <form onSubmit={submitHandle}>
+                <div>
+                    <h2>Sign In</h2>
+                    <label>Email: </label><br/>
+                    <input type='text' name="username" required />
+                </div>
+                <div>
+                    <label>Password: </label><br/>
+                    <input type='password' name="password" required />
+                </div>
+                <div className="button">
+                    <button type='submit'>Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+
+  return (
+    <div className='loginPage'>
+        {isSubmit ? SignInForm : SignInForm}
+    </div>
+  );
+}
+
+export default LoginPage
