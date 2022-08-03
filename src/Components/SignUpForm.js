9819ca9c1acf04add9b2318b1d47fd76ad3a2b3d
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-08-03 12:24:00 -0600

    Update SignUpForm.js

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,109 +1,51 @@
 import React, { Component } from "react";
 import "./StyleSignUpForm.css";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { FormGroup, Button } from "react-bootstrap";
 
-class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
+const SignUpForm = (props) => {
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().email("You have to enter an invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+  });
 
-    this.state = {
-      username: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    };
-  }
-
-  usernameHandle = (event) => {
-    this.setState({
-      username: event.target.value,
-    });
-  };
-  emailHandle = (event) => {
-    this.setState({
-      email: event.target.value,
-    });
-  };
-
-  passwordHandle = (event) => {
-    this.setState({
-      password: event.target.value,
-    });
-  };
-
-  confirmPasswordHandle = (event) => {
-    this.setState({
-      confirmPassword: event.target.value,
-    });
-  };
-
-  submitHandle = (event) => {
-    event.preventDefault();
-
-    if (`${this.state.password}` === `${this.state.confirmPassword}`) {
-      alert(`
-            Username: ${this.state.username}\n
-            Email: ${this.state.email} \n
-            Password: ${this.state.password} \n
-            Confirm Password: ${this.state.confirmPassword}`);
-    } else {
-      alert("Password does not match check your input");
-    }
-  };
-
-  render() {
-    return (
-      <div className="form">
-        <form onSubmit={this.submitHandle}>
-          <h1>Sign Up</h1>
-
-          <div>
-            <label>Username:</label>
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.usernameHandle}
-              required
-            />
+  console.log(props);
+  return (
+    <div className="regisForm">
+      <Formik {...props} validationSchema={validationSchema}>
+        <Form>
+          <FormGroup className="firstName">
+            First Name:
+            <Field name="first" type="text" className="field-control" />
+            <ErrorMessage name="first" className="d-block invalid-feedback" component="span" />
+          </FormGroup>
+          <FormGroup className="lastName">
+            Last Name:
+            <Field name="last" type="text" className="field-control" />
+            <ErrorMessage name="last" className="d-block invalid-feedback" component="span" />
+          </FormGroup>
+          <FormGroup className="firstName">
+            Email:
+            <Field name="email" type="email" className="field-control" />
+            <ErrorMessage name="first" className="d-block invalid-feedback" component="span" />
+          </FormGroup>
+          <FormGroup className="firstName">
+            password:
+            <Field name="password" type="password" className="field-control" />
+            <ErrorMessage name="password" className="d-block invalid-feedback" component="span" />
+          </FormGroup>
+          <div className="regisButton">
+            <button variant="danger" size="lg" block="block" type="submit">
+              {props.children}
+            </button>
           </div>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.emailHandle}
-              required
-            />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              onChange={this.passwordHandle}
-              value={this.state.password}
-              required
-            />
-          </div>
-          <div>
-            <label>Confirm password:</label>
-            <input
-              type="password"
-              name="confirmPassword"
-              value={this.state.confirmPasswordHandle}
-              onChange={this.confirmPasswordHandle}
-              required
-            />
-          </div>
-          <br />
-          <div className="buttons">
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
 export default SignUpForm;
