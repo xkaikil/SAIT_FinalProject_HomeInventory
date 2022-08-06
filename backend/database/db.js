4f3f672d5ae798d2fa07f136dc042bc2b573e6d9
lenard-villestas <lenard.villestas@outlook.com>
2022-08-06 15:31:50 -0600

    fixes db URL
    this will now make our posts go to the homenventory db isntead of test

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "mongodb+srv://group1:11JleR4pzFGH3n9P@homenventory.mvar6zd.mongodb.net/?retryWrites=true&w=majority"
+  db: "mongodb+srv://group1:11JleR4pzFGH3n9P@homenventory.mvar6zd.mongodb.net/homenventory?retryWrites=true&w=majority"
 };
 
 
