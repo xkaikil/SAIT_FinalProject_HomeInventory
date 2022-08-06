4a6c4d8cd8ae9dcf72c2d2d24a3d27d48b51596c
lenard-villestas <lenard.villestas@outlook.com>
2022-08-06 10:15:38 -0600

    updates homepage
    styling and look updates

--- src/App.js
+++ src/App.js
@@ -3,29 +3,6 @@ import "./App.css";
 
 import Navibar from "./Components/Navbar";
 
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-
-// function App() {
-//   return (
-//     // <Router>
-//     //   <div className="App">
-
-//     //     <Navibar user="Guest" loggedIn='false' />
-//     //     <Routes>
-//     //       <Route exact path="/" element={<RegisUser />} />
-
-//     //     </Routes>
-//     //   </div>
-//     // </Router>
-
-//     <div className="App">
-
-//       <Navibar user="Guest" loggedIn='false' />
-//       <RegisUser />
-//     </div>
-
-//   );
-// }
 
 const App = () => {
   return (
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,15 +1,29 @@
+import { Button } from "react-bootstrap";
 import {
     BrowserRouter as Router, Routes,
     Route, Link
   } from "react-router-dom";
-
+import "./homepage.css";
+import heroImg from "../assets/heroimage.jpg";
 const HomePage = () => {
     
     return (
-        <div>
-        <h2>Hello, welcome to HOME nVentory</h2>
-        <p>How to use here....</p>
-        <Link to="/signup">SignUp now for free</Link>
+        <div className="homepage-wrapper">
+
+        
+        <div className="description">
+        <div className="d-flex flex-column">
+            
+        <h1>Inventory your belongings with HOME nVentory</h1>
+        <h2>Find the right insurance that you need</h2>
+        <h2>Make filing claims hassle free</h2>
+        <h2>it's free and  easy to use!</h2>
+        </div>
+
+        <Link to="/signup"><Button className="hp_button">Start inventorying now!</Button></Link>
+
+        </div>
+
         </div>
     )
 }
