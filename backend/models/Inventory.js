8018853f8312c9f05a7809d12d7d42112074a9c6
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-08-09 17:14:29 -0600

    Update Inventory.js

--- backend/models/Inventory.js
+++ backend/models/Inventory.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let invSchema = new Schema (
+let invSchema = new Schema(
     {
         category: {
             type: String,
             required: true
-        }
-    },
-    {
+        },
         name: {
             type: String,
             required: true
-        }
-    },
-    {
+        },
         price: {
             type: Number, min: [0.0, 'Cannot be a negative number'],
             required: true
         }
-    }, {
+    },
+    {
         collection: 'inventory'
     }
 )
