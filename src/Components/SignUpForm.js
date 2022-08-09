fcf87c4af133d1376e6a0b6545d9260f9323fb9b
Aidenkopec <Aidenkopec@icloud.com>
2022-08-09 15:21:57 -0600

    Adding link from signUp to login
    Allows user to access login page from sign up page if an account is already created

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -3,45 +3,69 @@ import "./StyleSignUpForm.css";
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { FormGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const SignUpForm = (props) => {
   const validationSchema = Yup.object().shape({
     firstName: Yup.string().required("Required"),
     lastName: Yup.string().required("Required"),
-    email: Yup.string().email("You have to enter an invalid email address").required("Required"),
+    email: Yup.string()
+      .email("You have to enter an invalid email address")
+      .required("Required"),
     password: Yup.string().required("Required"),
   });
 
   console.log(props);
   return (
     <div className="regisForm">
-      <Formik {...props} validationSchema={validationSchema} initialValues={ {firstName: '', lastName: '', email: '', password: ''} }>
+      <Formik
+        {...props}
+        validationSchema={validationSchema}
+        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+      >
         <Form>
           <FormGroup className="firstName">
             First Name:
             <Field name="firstName" type="text" className="field-control" />
-            <ErrorMessage name="first" className="d-block invalid-feedback" component="span" />
+            <ErrorMessage
+              name="first"
+              className="d-block invalid-feedback"
+              component="span"
+            />
           </FormGroup>
           <FormGroup className="lastName">
             Last Name:
             <Field name="lastName" type="text" className="field-control" />
-            <ErrorMessage name="last" className="d-block invalid-feedback" component="span" />
+            <ErrorMessage
+              name="last"
+              className="d-block invalid-feedback"
+              component="span"
+            />
           </FormGroup>
           <FormGroup className="email">
             Email:
             <Field name="email" type="text" className="field-control" />
-            <ErrorMessage name="first" className="d-block invalid-feedback" component="span" />
+            <ErrorMessage
+              name="first"
+              className="d-block invalid-feedback"
+              component="span"
+            />
           </FormGroup>
           <FormGroup className="password">
             password:
             <Field name="password" type="password" className="field-control" />
-            <ErrorMessage name="password" className="d-block invalid-feedback" component="span" />
+            <ErrorMessage
+              name="password"
+              className="d-block invalid-feedback"
+              component="span"
+            />
           </FormGroup>
           <div className="regisButton">
             <Button variant="danger" size="lg" block="block" type="submit">
               {props.children}
             </Button>
           </div>
+          <Link to="/login">Already have an account? </Link>
         </Form>
       </Formik>
     </div>
