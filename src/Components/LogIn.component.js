1fd2ab0b9c09e549f7f04b46b477f93c8e0bab79
Aidenkopec <Aidenkopec@icloud.com>
2022-08-09 10:17:43 -0600

    Adding Basic Admin User management page
    Need's to be integrated into navbar. Think it would be best to be redirected to theses pages from the login page

--- src/Components/LogIn.component.js
+++ src/Components/LogIn.component.js
@@ -3,31 +3,34 @@ import axios from "axios";
 import LogInPageMaster from "./LogInPageMaster";
 
 const LogIn = () => {
-    const [formValues, setFormValues] = useState({
-        email: "",
-        password: "",
-    });
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const onSubmit = (accountObject) => {
-        axios
-            .post("http://localhost:4000/users/login", accountObject)
-            .then((res) => {
+  const onSubmit = (accountObject) => {
+    axios
+      .post("http://localhost:4000/users/login", accountObject)
+      .then((res) => {
+        if (res.status === 200) {
+          alert("Login successful, Welcome!");
+        } else Promise.reject();
+      })
+      .catch((err) => {
+        if (err.response) {
+          alert("Invalid Credentials");
+        } else alert("Something went wrong");
+      });
+  };
 
-                if (res.status === 200) {
-                    alert("Login successful!!");
-                }  else Promise.reject();
-            })
-            .catch((err) => {
-                if (err.response) {
-                    alert("Invalid Credentials");
-                } else
-                alert("Something went wrong")});
-    };
-
-    return (
-        <LogInPageMaster initialValue={formValues} onSubmit={onSubmit} enableReinitialize>
-            Log In
-        </LogInPageMaster>
-    );
-}
+  return (
+    <LogInPageMaster
+      initialValue={formValues}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      Log In
+    </LogInPageMaster>
+  );
+};
 export default LogIn;
