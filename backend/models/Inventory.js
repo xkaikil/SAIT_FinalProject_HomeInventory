bf19f1bce717277b45ff80a7503bd7414d5ca1b1
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-08-09 10:10:00 -0600

    Created inventory.js & inv.route.js

---
+++ backend/models/Inventory.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let invSchema = new Schema (
+    {
+        category: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        price: {
+            type: Number, min: [0.0, 'Cannot be a negative number'],
+            required: true
+        }
+    }, {
+        collection: 'inventory'
+    }
+)
+
+module.exports = mongoose.model('Inventory', invSchema);
