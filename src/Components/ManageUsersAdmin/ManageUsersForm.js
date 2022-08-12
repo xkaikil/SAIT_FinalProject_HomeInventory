e84ca920c6e3bae38b55f5e6b450812dca755c68
Aidenkopec <Aidenkopec@icloud.com>
2022-08-11 18:13:22 -0600

    Adding "Role" to admin table
    Displays users roles in the system

--- src/Components/ManageUsersAdmin/ManageUsersForm.js
+++ src/Components/ManageUsersAdmin/ManageUsersForm.js
@@ -1,55 +1,77 @@
 import React from "react";
 import * as Yup from "yup";
-import {Formik, Form, Field, ErrorMessage} from "formik";
-import {FormGroup, Button} from "react-bootstrap";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { FormGroup, Button } from "react-bootstrap";
 
 const ManageUsers = (props) => {
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().required("Required"),
-        lastName: Yup.string().required("Required"),
-        email: Yup.string().email("You have to enter an invalid email address").required("Required"),
-        password: Yup.string().required("Required"),
-    });
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string()
+      .email("You have to enter an invalid email address")
+      .required("Required"),
+    password: Yup.string().required("Required"),
+  });
+  //   initialValues={{firstName: '', lastName: '', email: '', password: '', role:'user'}}
 
-    console.log(props);
-    return (
-        <div className="regisForm">
-            <Formik {...props} validationSchema={validationSchema}
-                    initialValues={{firstName: '', lastName: '', email: '', password: '', role:'user'}}>
-                <Form>
-                    <FormGroup className="firstName">
-                        First Name:
-                        <Field name="firstName" type="text" className="field-control"/>
-                        <ErrorMessage name="first" className="d-block invalid-feedback" component="span"/>
-                    </FormGroup>
-                    <FormGroup className="lastName">
-                        Last Name:
-                        <Field name="lastName" type="text" className="field-control"/>
-                        <ErrorMessage name="last" className="d-block invalid-feedback" component="span"/>
-                    </FormGroup>
-                    <FormGroup className="email">
-                        Email:
-                        <Field name="email" type="text" className="field-control"/>
-                        <ErrorMessage name="first" className="d-block invalid-feedback" component="span"/>
-                    </FormGroup>
-                    <FormGroup className="password">
-                        password:
-                        <Field name="password" type="password" className="field-control"/>
-                        <ErrorMessage name="password" className="d-block invalid-feedback" component="span"/>
-                    </FormGroup>
-                    <FormGroup className="role">
-                        role:
-                        <Field name="role" type="text" className="field-control"/>
-                        <ErrorMessage name="role" className="d-block invalid-feedback" component="span"/>
-                    </FormGroup>
-                    <div className="regisButton">
-                        <Button variant="danger" size="lg" block="block" type="submit">
-                            {props.children}
-                        </Button>
-                    </div>
-                </Form>
-            </Formik>
-        </div>
-    );
+  console.log(props);
+  return (
+    <div className="regisForm">
+      <Formik {...props} validationSchema={validationSchema}>
+        <Form>
+          <FormGroup className="firstName">
+            First Name:
+            <Field name="firstName" type="text" className="field-control" />
+            <ErrorMessage
+              name="first"
+              className="d-block invalid-feedback"
+              component="span"
+            />
+          </FormGroup>
+          <FormGroup className="lastName">
+            Last Name:
+            <Field name="lastName" type="text" className="field-control" />
+            <ErrorMessage
+              name="last"
+              className="d-block invalid-feedback"
+              component="span"
+            />
+          </FormGroup>
+          <FormGroup className="email">
+            Email:
+            <Field name="email" type="text" className="field-control" />
+            <ErrorMessage
+              name="first"
+              className="d-block invalid-feedback"
+              component="span"
+            />
+          </FormGroup>
+          <FormGroup className="password">
+            password:
+            <Field name="password" type="password" className="field-control" />
+            <ErrorMessage
+              name="password"
+              className="d-block invalid-feedback"
+              component="span"
+            />
+          </FormGroup>
+          <FormGroup className="role">
+            role:
+            <Field name="role" type="text" className="field-control" />
+            <ErrorMessage
+              name="role"
+              className="d-block invalid-feedback"
+              component="span"
+            />
+          </FormGroup>
+          <div className="regisButton">
+            <Button variant="danger" size="lg" block="block" type="submit">
+              {props.children}
+            </Button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 export default ManageUsers;
--- src/Components/ManageUsersAdmin/UserTableRow.js
+++ src/Components/ManageUsersAdmin/UserTableRow.js
@@ -1,40 +1,40 @@
 import React from "react";
-import {Button} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const UserTableRow = (props) => {
-    const {_id, firstName, lastName, email, password} = props.obj;
+  const { _id, firstName, lastName, email, password, role } = props.obj;
 
-    const deleteUser = () => {
-        axios
-            .delete("http://localhost:4000/admin/delete-user/" + _id)
-            .then((res) => {
-                if (res.status === 200) {
-                    alert("User Successfully deleted");
-                    window.location.reload();
-                } else Promise.reject();
-            })
-            .catch((err) => alert("Something went wrong"));
-    }
+  const deleteUser = () => {
+    axios
+      .delete("http://localhost:4000/admin/delete-user/" + _id)
+      .then((res) => {
+        if (res.status === 200) {
+          alert("User Successfully deleted");
+          window.location.reload();
+        } else Promise.reject();
+      })
+      .catch((err) => alert("Something went wrong"));
+  };
 
-    return (
-        <tr>
-            <td>{firstName}</td>
-            <td>{lastName}</td>
-            <td>{email}</td>
-            <td>{password}</td>
-            <td>
-                <Link className="edit-link" to={"/admin/edit-user/" + _id}>
-                    Edit
-                </Link>
-                <Button onClick={deleteUser} size="sm" variant="danger">
-                    Delete
-                </Button>
-            </td>
-        </tr>
-    );
-
+  return (
+    <tr>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{email}</td>
+      <td>{password}</td>
+      <td>{role}</td>
+      <td>
+        <Link className="edit-link" to={"/admin/edit-user/" + _id}>
+          Edit
+        </Link>
+        <Button onClick={deleteUser} size="sm" variant="danger">
+          Delete
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default UserTableRow;
